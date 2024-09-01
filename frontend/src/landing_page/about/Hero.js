8f23fd135e315a8249cfebe332d2cb0705e1dfa5
subhas-pramanik-09 <subhaspramanik864@gmail.com>
2024-09-01 18:12:24 +0530

    About updated

--- frontend/src/landing_page/about/Hero.js
+++ frontend/src/landing_page/about/Hero.js
@@ -5,7 +5,7 @@ function Hero() {
     <div className="container">
       <div className="row p-5 mt-5 mb-5">
         <h1 className="fs-2 text-center">
-          We pioneered the discount broking model in India
+          We pioneered the discount broking model in India !
           <br />
           Now, we are breaking ground with our technology.
         </h1>
